b216c88d30cde24786953610a8a786b712b0e624
YugYt <72304225+YugYt@users.noreply.github.com>
2021-02-25 11:21:45 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,75 @@
+class Game{
+    constructor(){
+
+    }
+    getState() {
+        var gameStateRef = database.ref('gameState');
+        gameStateRef.on("value", function (data) {
+            gameState = data.val();
+        })
+
+    }
+
+    update(state) {
+        database.ref('/').update({
+            gameState: state
+        });
+    }
+    async start() {
+            if (gameState === 0) {
+                player = new Player();
+                var playerCountRef = await database.ref('playerCount').once("value");
+                if (playerCountRef.exists()) {
+                    playerCount = playerCountRef.val();
+                    player.getCount();
+                }
+                form = new Form()
+                form.display();
+            }
+    player1 = createSprite(200,500);
+    player1.addImage("player1",player_img);
+    
+    player2 = createSprite(800,500);
+    player2.addImage("player2", player_img);
+    players=[player1,player2];
+
+        }
+    
+    play(){
+        
+        form.hide();
+
+        Player.getPlayerInfo();
+        image(back_img, 0, 0, 1000, 800);
+        var x =100;
+        var y=200;
+        var index =0;
+        drawSprites();
+
+        for(var plr in allPlayers){
+        
+            index = index+1;
+            x = 500-allPlayers[plr].distance;
+            y=500;
+            
+            players[index -1].x = x;
+            players[index - 1].y = y;
+
+            // Differentiate the main player by printing
+            // the name of the player on the basket. 
+
+        }
+
+
+        // Give movements for the players using arrow keys
+
+
+        // Create and spawn fruits randomly
+
+        
+    }
+
+    end(){
+       console.log("Game Ended");
+    }
+}
